1dca7571e0d35dc62d38dd0b4c1466f115577090
Joshua Coquia <mail@joshuacoquia.com>
2025-09-23 22:05:41 -0700

    feat: install react-grid-layout

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,9 +2,10 @@ import { experimental_AstroContainer as AstroContainer } from 'astro/container';
 import { expect, test } from 'vitest';
 import indexPage from '../src/pages/index.astro';
 
-test('index.html', async () => {
+test('index.astro', async () => {
   const container = await AstroContainer.create();
   const result = await container.renderToString(indexPage);
+  console.log(result)
 
   expect(result).toContain('Homepage Editor');
 });
