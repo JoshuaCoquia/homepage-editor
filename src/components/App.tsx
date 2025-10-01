e7786c7ef4574af25a1f75870f440365ffc80f10
Joshua Coquia <mail@joshuacoquia.com>
2025-10-01 16:51:41 -0700

    feat: allow users to create and delete widgets using buttons

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,12 +6,31 @@ interface Widget {
 
 }
 
-export default function App({ widgets }: { widgets: Widget[] | null }) {
-    const [widgetList, setWidgetList] = useState<Widget[] | null>(widgets);
-    return <ul>
-        {widgetList?.map(widget => <li key={widget.id}>
-            <h2>{widget.title}</h2>
-            <p>{widget.description}</p>
-        </li>)}
-    </ul>
+interface Props {
+    widgets?: Widget[];
+}
+
+export default function App({ widgets = [] }: Props) {
+    const [widgetList, setWidgetList] = useState<Widget[]>(widgets);
+
+    function addWidget(title: string, description: string) {
+        const newWidget: Widget = {
+            id: (Math.random() * 1000000).toFixed(0),
+            title,
+            description
+        };
+        setWidgetList([...widgetList!, newWidget]);
+    }
+
+    return <>
+        <button onClick={() => addWidget("New Widget", "New Widget")}>Add Widget</button>
+        <ul>
+            {widgetList?.map(widget => <li key={widget.id}>
+                <h2>{widget.title}</h2>
+                <small>ID: {widget.id}</small>
+                <br />
+                <button onClick={() => setWidgetList(widgetList.filter(w => w.id !== widget.id))}>Remove</button>
+            </li>)}
+        </ul>
+    </>
 }
