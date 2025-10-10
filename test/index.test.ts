ed321afccba5f9113c51ce6bd70d5fb898467fd5
Joshua Coquia <mail@joshuacoquia.com>
2025-10-10 11:56:05 -0700

    test(index.test.ts): include react renderer

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,11 +1,13 @@
 import { experimental_AstroContainer as AstroContainer } from 'astro/container';
 import { expect, test } from 'vitest';
 import indexPage from '../src/pages/index.astro';
+import { getContainerRenderer as reactRenderer } from "@astrojs/react";
+import { loadRenderers } from "astro:container";
 
 test('index.astro', async () => {
-  const container = await AstroContainer.create();
+  const renderers = await loadRenderers([reactRenderer()]);
+  const container = await AstroContainer.create({ renderers });
   const result = await container.renderToString(indexPage);
-  console.log(result)
 
   expect(result).toContain('Homepage Editor');
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,10 @@
+// @vitest-environment node
 /// <reference types="vitest" />
 import { getViteConfig } from 'astro/config';
+import type { UserConfig } from 'vite';
+import react from "@vitejs/plugin-react";
 
 export default getViteConfig({
+	plugins: [react()],
 	test: {},
-});
+} as UserConfig);
