94c52bfebbdecb0b12dd54282f697118054c76ea
Joshua Coquia <mail@joshuacoquia.com>
2025-10-10 12:39:47 -0700

    test(index.test.ts): use describe and force node test env

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,13 +1,21 @@
+// @vitest-environment node
 import { experimental_AstroContainer as AstroContainer } from 'astro/container';
-import { expect, test } from 'vitest';
-import indexPage from '../src/pages/index.astro';
+import { describe, it, expect } from 'vitest';
 import { getContainerRenderer as reactRenderer } from "@astrojs/react";
 import { loadRenderers } from "astro:container";
 
-test('index.astro', async () => {
-  const renderers = await loadRenderers([reactRenderer()]);
-  const container = await AstroContainer.create({ renderers });
-  const result = await container.renderToString(indexPage);
+import indexPage from '../src/pages/index.astro';
 
-  expect(result).toContain('Homepage Editor');
+describe('index.astro', () => {
+  it('renders the initial page', async () => {
+    const renderers = await loadRenderers([reactRenderer()]);
+    const container = await AstroContainer.create({ renderers });
+    const result = await container.renderToString(indexPage);
+
+    // Astro Rendering (index.astro)
+    expect(result).toContain('Homepage Editor');
+
+    // React Rendering (App.tsx)
+    expect(result).toContain('Add Widget');
+  });
 });
